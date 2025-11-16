50625d31ae6b179481643aeb939f569a49c99b02
myethg <97392455+myethg@users.noreply.github.com>
2025-11-16 00:53:03 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,20 @@
--- dont printz at 3am
+local player = game:Players.LocalPlayer
+local placeid = game.PlaceId
+
+local base = "https://raw.githubusercontent.com/myethg/FluentCLI-SS/main/games/"
+local url = base .. tostring(placeid) .. ".luau"
+
+local success, result = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if success and result and not result:find("404") then
+    local load_success, error_msg = pcall(function()
+        loadstring(result)()
+    end)
+    
+    if not load_success then
+        warn("script failed to execute: " .. tostring(error_msg))
+    end
+end
+-- shit loader
